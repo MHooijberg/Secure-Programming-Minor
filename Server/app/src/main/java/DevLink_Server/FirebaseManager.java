e4f6fd25c2b42166102f22f22cc668e2a7f27564
Mark Hooijberg <m.hooijberg24@gmail.com>
2022-12-13 19:33:26 +0100

    Created Code Base for the server

---
+++ Server/app/src/main/java/DevLink_Server/FirebaseManager.java
@@ -0,0 +1,14 @@
+package devlink_server;
+
+
+
+public class FirebaseManager {
+
+    public FirebaseManager(){
+        
+    }
+
+    public void InitializeFirebase() throws UnsupportedOperationException{
+        // TODO: Create a method to initialize and connect to the different Firebase Services.
+    }
+}
