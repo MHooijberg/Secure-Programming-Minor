5ed68dc94af5eb488417abf0ce8948094506a14e
Georgi768 <59054341+Georgi768@users.noreply.github.com>
2022-12-19 13:14:34 +0100

    Added login system with email verification

--- Client/app/src/main/java/com/example/chatsystemfordevs/Controller/GuildServerController.java
+++ Client/app/src/main/java/com/example/chatsystemfordevs/Controller/GuildServerController.java
@@ -1,11 +1,23 @@
 package com.example.chatsystemfordevs.Controller;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.chatsystemfordevs.Model.GuildServerModel;
+import com.example.chatsystemfordevs.R;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class GuildServerController {
+public class GuildServerController extends AppCompatActivity {
     private FirebaseFirestore database;
     private FirebaseMessagingService firebaseMessaging;
     private GuildServerModel guildModel;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guildserverview);
+    }
 }
--- Client/app/src/main/java/com/example/chatsystemfordevs/Utilities/AuthenticationManager.java
+++ Client/app/src/main/java/com/example/chatsystemfordevs/Utilities/AuthenticationManager.java
@@ -1,12 +1,20 @@
 package com.example.chatsystemfordevs.Utilities;
 
+import static com.google.firebase.appcheck.internal.util.Logger.TAG;
+
 import android.app.Activity;
-import android.telephony.PhoneNumberUtils;
+import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+
+import com.example.chatsystemfordevs.Controller.GuildServerController;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class AuthenticationManager {
@@ -26,24 +34,45 @@ public class AuthenticationManager {
         firebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(activity, task -> {
                     if (task.isSuccessful() && firebaseAuth.getCurrentUser() != null) {
-                        database.createUser(username, email,phoneNumber);
+                        database.createUser(username, email, phoneNumber);
                     } else {
                         System.out.println(task.getException().getMessage());
                     }
                 });
     }
 
-    public boolean isEmailValid(String email){
+    public void loginUsername(Activity activity,String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(activity, task -> {
+                    if (task.isSuccessful()) {
+                        if(Objects.requireNonNull(this.firebaseAuth.getCurrentUser()).isEmailVerified()){
+                            Intent loginPage = new Intent(activity, GuildServerController.class);
+                            activity.startActivity(loginPage);
+                            Log.d(TAG, "signInWithEmail:success");
+                        }else{
+                            Toast.makeText(activity, "The email needs to be verified before the app can be used", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Log.d(TAG, task.getException().getMessage());
+                    }
+                });
+    }
+
+    public void resetPassword(String email, String password) {
+
+    }
+
+
+    public boolean isEmailValid(String email) {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
 
     }
 
-    public boolean isPhoneNumberValid(String phoneNumber){
+    public boolean isPhoneNumberValid(String phoneNumber) {
         return phoneNumber.length() >= 10 && Patterns.PHONE.matcher(phoneNumber).matches();
     }
 
-    public boolean isPasswordEqual(String password, String repeatedPassword){
-        return TextUtils.equals(password,repeatedPassword);
+    public boolean isPasswordEqual(String password, String repeatedPassword) {
+        return TextUtils.equals(password, repeatedPassword);
     }
 
     public boolean isPasswordValidated(@NonNull String password) {
@@ -66,21 +95,10 @@ public class AuthenticationManager {
             } else if (Character.isUpperCase(c)) {
                 hasCapital = true;
             }
-            if(hasNum && hasLowChar && hasCapital && !pattern.matcher(password).matches()){
+            if (hasNum && hasLowChar && hasCapital && !pattern.matcher(password).matches()) {
                 return true;
             }
         }
         return false;
     }
-
-
-    public void signInUser(String email, String password) {
-
-    }
-
-    public void resetPassword(String email, String password) {
-
-    }
-
-
 }
