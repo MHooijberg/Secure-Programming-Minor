5fd644b6e1d999c481f4c7ae0ff92490c4c79118
Martin Westerhof <a.m.westerhof@gmail.com>
2022-12-23 16:01:50 +0100

    CBSManager Add Url to Json Convert
    Setting functions

--- Server/app/src/main/java/DevLink_Server/CBSManager.java
+++ Server/app/src/main/java/DevLink_Server/CBSManager.java
@@ -1,7 +1,37 @@
 package DevLink_Server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class CBSManager {
 
-    
+    private String baseUrl = "https://opendata.cbs.nl/ODataApi/OData/";
+
+    public void CBSManager(){
+        // TODO: Create a constructor.
+    }
+
+    public static void getJsonFromUrl(String Dataset) throws MalformedURLException
+    {
+        String combineUrl = baseUrl + Dataset;
+        URL url = new URL(combineUrl);
+
+        StringBuilder builder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream(), UTF_8))) {
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                builder.append(str);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String jsonStr = builder.toString();
+        System.out.println(jsonStr);
+    }
 
 }
